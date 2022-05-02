e50efbd75e69d39cdfab8aa1b69e9165822373e9
MoyanPan <624870770a@gmail.com>
2022-05-02 15:24:46 +1200

    Exercise 3 Done

--- 2. Python workshop/Exercise1.py
+++ 2. Python workshop/Exercise1.py
@@ -1,12 +1,14 @@
 import csv
+from email import message
 from pprint import pprint
+from pyexpat.errors import messages
 transmissions = []
 with open("transmissions.csv","r") as file:
     for row in file:
         row = row.strip()
         transmissions.append(row.split(","))
 transmissions.pop(0)
-ids = [x[0] for x in transmissions]
+ids = [int(x[0]) for x in transmissions]
 transdetail = [x[1] for x in transmissions]
 sums = []
 for x in transmissions:
@@ -18,8 +20,11 @@ for x in transmissions:
         except:
             pass
     sums.append(num)
-pprint(sums)
-
-
+ASCII = [[ids[i],sums[i]] for i in range(len(sums))]
+ASCII = sorted(ASCII, key = lambda x:x[0])
+messages = ""
+for x in ASCII:
+    messages += chr(x[1])
+print(messages)
 
 
